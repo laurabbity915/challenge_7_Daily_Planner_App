652f8e5ab8b04c01e5e7757732c77e8e0441b09c
laurabbity915 <laura_yuen915@outlook.com>
2023-01-31 02:26:52 +0000

    change to use js to generate the timeblock and adding logic

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,46 @@
 //use moment.js to add current date
-var day = moment().format('dddd MMM Do');
-$("#currentDay").text(day);
+$(window).ready(function () {
+    var day = moment().format('dddd MMM Do');
+    $("#currentDay").text(day);
+    generateTimeBlock();
+    loadSavedEvent();
+});
 
+function generateTimeBlock() {
+    let htmlText = "";
+    for (let i = 0; i < timeBlock.length; i++) {
+        htmlText += "<div class='grid-item'>" + timeBlock[i] + "</div>";
+        htmlText += "<div class='grid-item'>";
+        htmlText += "<textarea id='" + timeBlock[i] + "'></textarea>";
+        htmlText += "</div>";
+        htmlText += "<div class='grid-item'>";
+        htmlText += "<button class='saveBtn' onclick='saveEvent(\"" + timeBlock[i] + "\")'>Save</button>";
+        htmlText += "</div>";
+    }
+    $(".container").html(htmlText);
+}
+
+// function for saving event by clicking the save button
+function saveEvent(eventId) {
+
+    saveToStorage(eventId,$("#" + eventId).val());
+    console.log($("#" + eventId).val());
+}
+
+// function for load saved event
+function loadSavedEvent(){
+
+    getFromStorage()
+}
+
+// function for geting local storage content
+function getFromStorage(key) {
+    // localStorage.getItem("1PM")
+    return JSON.parse(localStorage.getItem(key));
+}
+
+// function for saving local storage content
+function saveToStorage(key, content) {
+    //localStorage.setItem("1PM", "Meeting with me");
+    localStorage.setItem(key, JSON.stringify(content));
+}
--- starter/timeblock.js
+++ starter/timeblock.js
@@ -2,3 +2,16 @@ let appointment = {
     eventName: "",
     timeblock: new Date()
 }
+
+const timeBlock = [
+    "9AM",
+    "10AM",
+    "11AM",
+    "12PM",
+    "1PM",
+    "2PM",
+    "3PM",
+    "4PM",
+    "5PM"
+];
+
